fe6ba9c81a7f86ab804be406eae9786e5581c24d
Ivan Gordeychik <ivangordejci@gmail.com>
2021-06-15 12:11:24 +0300

    artistCreator done

--- force-app/main/default/lwc/artistCreator/artistCreator.js
+++ force-app/main/default/lwc/artistCreator/artistCreator.js
@@ -1,7 +1,7 @@
-import {LightningElement, api} from 'lwc';
+import {LightningElement} from 'lwc';
 import {reduceErrors} from 'c/ldsUtils';
 import {ShowToastEvent} from 'lightning/platformShowToastEvent';
-import createUser from '@salesforce/apex/UserController.createUser';
+import createContactWithUser from '@salesforce/apex/ArtistHelper.createContactWithUser';
 import NAME_FIELD from '@salesforce/schema/Contact.Name';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
@@ -9,63 +9,56 @@ import BIRTHDATE_FIELD from '@salesforce/schema/Contact.Birthdate';
 import CONTACT_OBJECT from '@salesforce/schema/Contact';
 
 export default class ArtistCreator extends LightningElement {
-    @api recordId;
     object = CONTACT_OBJECT;
     nameField = NAME_FIELD;
     emailField = EMAIL_FIELD;
     phoneField = PHONE_FIELD;
     birthdateField = BIRTHDATE_FIELD;
 
-    handleCreate(event) {
-        const evt = new ShowToastEvent({
-            variant: "success",
-            message: "Contact record {0} created!",
-            messageData: [
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('handleSubmit' + JSON.stringify(event.detail.fields));
+        createContactWithUser({con: event.detail.fields})
+            .then(result => {
+                console.log('result' + JSON.stringify(result));
+                let evt = new ShowToastEvent({
+                    variant: "success",
+                    message: "Contact record {0} created!",
+                    messageData: [
 
-                {
-                    url: `/${event.detail.id}`,
-                    label: event.detail.id
-                }
-            ]
-        });
-        this.dispatchEvent(evt);
-        this.resetFields();
-        createUser({contactId: event.detail.id})
-            .then(id => {
-                const evt = new ShowToastEvent({
+                        {
+                            url: `/${result[0]}`,
+                            label: result[0]
+                        }
+                    ]
+                });
+                this.dispatchEvent(evt);
+                 evt = new ShowToastEvent({
                     variant: "success",
                     message: "User record {0} created!",
                     messageData: [
 
                         {
-                            url: `/${id}`,
-                            label: id
+                            url: `/${result[1]}`,
+                            label: result[1]
                         }
                     ]
                 });
                 this.dispatchEvent(evt);
+                this.resetFields();
             })
             .catch(error => {
                 console.log('error' + JSON.stringify(error))
                 console.log(reduceErrors(error))
                 const evt = new ShowToastEvent({
-
                     variant: 'error',
-                    message: "Record error : {0}!",
-                    messageData: [
-
-
-                        {
-                            label: error.body.message
-                        }
-                    ]
-
+                    message: `Error: ${error.body.message}`
                 })
                 this.dispatchEvent(evt);
             });
-
-
     }
+
+
 
     resetFields() {
         const inputFields = this.template.querySelectorAll(
